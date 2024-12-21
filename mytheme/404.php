8dc8b55372862d26a8b08baf19a39ccc0b0a5c88
ryzhkoffVXN <i.ryzhkofff@gmail.com>
2024-12-21 20:48:13 +0300

    Add files via upload

---
+++ mytheme/404.php
@@ -0,0 +1,8 @@
+<?php get_header(); ?>
+<main style="text-align: center; padding: 50px;">
+    <h1 style="font-size: 48px; color: red; font-family: Brush Script MT, Brush Script Std, cursive;">Ошибка 404</h1>
+    <p style="font-size: 20px; color: white; font-family: Brush Script MT, Brush Script Std, cursive;">К сожалению, запрашиваемая страница не найдена.</p>
+    <img src="<?php echo get_template_directory_uri(); ?>/78c8db9s-960.jpg" style="max-width: 300px; margin: 20px auto;">
+    <p style="font-size: 18px; color: white; font-family: Brush Script MT, Brush Script Std, cursive;">Вернитесь на <a href="<?php echo home_url(); ?>" style="color: #0073aa;">главную страницу</a>.</p>
+</main>
+
